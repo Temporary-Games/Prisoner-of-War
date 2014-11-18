e1895ecddcd48cada12bf3ec0234006a8b022640
asc2 <asc2@10.9.30.1>
2014-11-18 13:25:33 -0600

    Updated Event class, comments in

--- PoW/src/pow/Event.java
+++ PoW/src/pow/Event.java
@@ -15,11 +15,25 @@ public class Event {
 	/*
 	 * If this is true, the event will display the title and
 	 * instantly free a prisoner.  The card will not be added
-	 * to the Player's inventory
+	 * to the Player's inventory.
 	 * 
 	 */
 	boolean freesAPrisoner;
+	
+	/*
+	 * If this is true, the card can be stored in your inventory
+	 * and used when a "bribable" card comes up.  (E.g. "Chocolate Bar"
+	 * would be a bribe and could be used on an "Aggressive Guard" card
+	 * which would be "bribable")
+	 * 
+	 */
 	boolean bribe;
+	
+	/*
+	 * If this is true the card can be used to free a prisoner when
+	 * provided with a "bribe" card.  See "bribe" comment for description
+	 * 
+	 */
 	boolean bribable;
 	
 	public Event(){
