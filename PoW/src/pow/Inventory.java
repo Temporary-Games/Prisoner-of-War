d32f8be676a57b3d0fdfa4ca824caad392b9100d
asc2 <asc2@10.9.34.51>
2014-11-19 13:12:27 -0600

    Beginning of Main and Inventory constructor with addItem method

---
+++ PoW/src/pow/Inventory.java
@@ -0,0 +1,24 @@
+package pow;
+
+public class Inventory {
+	//inventory instantiation
+	public int[] inventory = new int[4];
+	//adds item if event
+	public Inventory(int a1, int a2, int a3, int a4, int a5)
+	{
+		inventory[0] = a1;
+		inventory[1] = a2;
+		inventory[2] = a3;
+		inventory[3] = a4;
+		inventory[4] = a5;
+	}
+	public void addItem(int item)
+	{
+		for(int x = 0; x < 4; x++)
+		{
+			if(inventory[x] == 0)
+				inventory[x] = item;
+		}
+	}
+
+}
---
+++ PoW/src/pow/Main.java
@@ -0,0 +1,7 @@
+package pow;
+
+public class Main {
+	//rounds of game
+	private int rounds = 0;
+
+}
